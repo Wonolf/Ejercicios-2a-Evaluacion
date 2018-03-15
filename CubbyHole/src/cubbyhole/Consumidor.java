58bcfb31f22ea2af2df35988bd7974a5ec4fcfad
Bonoso Sierra <34323286+Wonolf@users.noreply.github.com>
2018-03-15 21:04:42 +0100

    Add files via upload

---
+++ CubbyHole/src/cubbyhole/Consumidor.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cubbyhole;
+
+/**
+ *
+ * @author Bonoso
+ */
+public class Consumidor extends Thread {
+    private CubbyHole cubbyhole;
+    private int numero;
+
+    public Consumidor(CubbyHole c, int numero) {
+        cubbyhole = c;
+        this.numero = numero;
+    }
+
+    @Override
+    public void run() {
+        int value = 0;
+        for (int i = 0; i < 10; i++) {
+            value = cubbyhole.get();
+            System.out.println("Consumidor saca:"+value);
+            try {
+            sleep(numero);
+            } catch (InterruptedException e) {}
+        }
+    }
+}
